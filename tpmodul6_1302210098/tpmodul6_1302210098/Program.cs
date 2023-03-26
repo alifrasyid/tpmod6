8e441aaadc41d83e6a1410605a9ad0c0839cb780
alifrasyid <alifrasyid.271003@gmail.com>
2023-03-26 23:07:52 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302210098/tpmodul6_1302210098/Program.cs
+++ tpmodul6_1302210098/tpmodul6_1302210098/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random random = new Random();
+        id = random.Next(10000, 99999);
+        this.title = title;
+        playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("ID: " + id);
+        Console.WriteLine("Title: " + title);
+        Console.WriteLine("Play Count: " + playCount);
+    }
+
+    public static void Main(string[] args)
+    {
+        string judulVideo = "Tutorial Design By Contract - Alif";
+        SayaTubeVideo video = new SayaTubeVideo(judulVideo);
+        video.IncreasePlayCount(20);
+        video.PrintVideoDetails();
+    }
+}
